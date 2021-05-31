6d7389f4158afd83e6c3d74008286fcc11758db8
Bobby T <bob.tarino@gmail.com>
2021-05-31 13:15:54 -0500

    replaced app with router in filesnot in root of project

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const { createNewNote, deleteNote } = require('../../lib/animals')
 
-app.get('/api/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
-app.post('/api/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     const newNote = createNewNote(req.body, allNotes);
     res.json(newNote);
 });
 
-app.delete('/api/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res) => {
     deleteNote(req.params.id, allNotes);
     res.json(true);
 });
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,16 +2,16 @@ const path = require('path');
 const router = require('express').Router();
 
 // make GET routes that serve front-end application to the client
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-app.get('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 // wildcard route for requests that don't exist - redirects to homepage // route should always come last
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
